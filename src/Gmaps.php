663ecf8fd7727195d8035ee1564364733c093356
Thomas Elvin <info@datalaere.dk>
2019-03-08 10:49:29 +0100

    Update
    Changed namespace

--- src/Gmaps.php
+++ src/Gmaps.php
@@ -1,58 +1,48 @@
 <?php
 
-namespace WebSupportDK\PHPApi;
+namespace Datalaere\PHPApi;
 
 class Gmaps
 {
+    private $api;
+    private $center;
+    private $zoom;
+    private $marker;
+    private $listener = array();
+    private $position = array();
+    private $map;
 
-    private
-            $api,
-            $center,
-            $zoom,
-            $marker,
-            $listener = array(),
-            $position = array(),
-            $map;
-
-    public
-            function setApi($params = '')
+    public function setApi($params = '')
     {
         $this->api = "<script src='https://maps.googleapis.com/maps/api/js" . $params . "' ></script>";
     }
 
-    public
-            function startScript()
+    public function startScript()
     {
         echo "<script>";
     }
 
-    public
-            function endScript()
+    public function endScript()
     {
         echo "</script>";
     }
 
-    public
-            function setCenter($lat, $lng)
+    public function setCenter($lat, $lng)
     {
         $this->center = "{lat: $lat, lng: $lng}";
     }
 
-    public
-            function setZoom($zoom = 10)
+    public function setZoom($zoom = 10)
     {
         $this->zoom = $zoom;
     }
 
-    public
-            function setPosition($name, $lat, $lng)
+    public function setPosition($name, $lat, $lng)
     {
-
         $this->position = array_merge($this->position, array($name => "{lat: $lat, lng: $lng}"));
     }
 
-    public
-            function setFunction($name, $param, $code)
+    public function setFunction($name, $param, $code)
     {
         $function = <<<FUNCTION
       function $name($param){
@@ -62,8 +52,7 @@ FUNCTION;
         return $function;
     }
 
-    public
-            function setListener($name, $postition, $function, $callback = 'click')
+    public function setListener($name, $postition, $function, $callback = 'click')
     {
         $listener       = <<<Listener
      $name.addListener('$callback', function () {
@@ -73,8 +62,7 @@ Listener;
         $this->listener = array_merge($this->listener, $listener);
     }
 
-    public
-            function setMarker($title, $name)
+    public function setMarker($title, $name)
     {
         $position       = $this->position[$name];
         $marker         = <<<MARKER
@@ -87,8 +75,7 @@ MARKER;
         $this->marker[] = $marker;
     }
 
-    public
-            function setMap($element_id = 'map')
+    public function setMap($element_id = 'map')
     {
         $map       = <<<MAP
             var map = new google.maps.Map(document.getElementById('$element_id'), {
@@ -99,18 +86,15 @@ MAP;
         $this->map = $map;
     }
 
-    public
-            function initMap($extra = '')
+    public function initMap($extra = '')
     {
         echo $this->api;
         $this->startScript();
         echo $this->map;
-        foreach ($this->marker as $marker)
-        {
+        foreach ($this->marker as $marker) {
             echo $marker;
         }
         echo $extra;
         $this->endScript();
     }
-
 }
